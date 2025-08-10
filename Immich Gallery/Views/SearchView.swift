3b0d9e077751c11a869677f025fa547de4ea36ca
mensadilabs <21.biscuits_lyre@icloud.com>
2025-08-10 15:59:38 -0700

    Fix calude schenanigans

--- Immich Gallery/Views/SearchView.swift
+++ Immich Gallery/Views/SearchView.swift
@@ -174,13 +174,3 @@ struct SearchView: View {
         }
     }
 }
-
-// Button style that matches the existing CardButtonStyle used in other views
-struct CardButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
-            .opacity(configuration.isPressed ? 0.8 : 1.0)
-            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
-    }
-}
